1b87a8872dd9ab04893992857df0a1161303ece5
Chris Liu <acciochris@163.com>
2021-04-05 12:43:13 +0800

    Add light controller.

--- intellilight.ino
+++ intellilight.ino
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <Keypad.h>
 #include <LiquidCrystal_I2C.h>
-#include <stdio.h>
 
 // Keypad rows and columns
 #define ROWS 4
@@ -9,6 +8,8 @@
 
 // Photoresistor
 #define PHOTOR A0
+#define VMIN 400
+#define VMAX 800
 
 // Modes
 #define TIME 0
@@ -17,6 +18,11 @@
 // LCD width and height
 #define LCD_WIDTH 16
 #define LCD_HEIGHT 2
+
+// Lights
+#define RED 9
+#define GREEN 10
+#define BLUE 11
 
 // LCD
 LiquidCrystal_I2C lcd(0x27, LCD_WIDTH, LCD_HEIGHT);
@@ -27,16 +33,21 @@ const char keys[ROWS][COLS] = {{'1', '2', '3', 'A'},
                                {'7', '8', '9', 'C'},
                                {'*', '0', '#', 'D'}};
 
-const byte rowPins[ROWS] = {9, 8, 7, 6};
+const byte rowPins[ROWS] = {12, 8, 7, 6};
 const byte colPins[COLS] = {5, 4, 3, 2};
 
 Keypad keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 // Mode
 int mode = LIGHT;
-int timeout = 0;
+unsigned long timeout = 0;
+unsigned long start_time;
 
 void setup() {
+  Serial.begin(115200);
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
   lcd.init();
   lcd.backlight();
   displayStatus();
@@ -46,6 +57,7 @@ void loop() {
   char key;
   static bool selecting_mode;
   static bool setting_time;
+  static bool is_on;
 
   key = keypad.getKey();
   if (key != NO_KEY) {
@@ -67,7 +79,7 @@ void loop() {
         // Reset variables
         selecting_mode = false;
         setting_time = true;
-        timeout = 0;
+        timeout = 0UL;
 
         lcd.clear();
         lcd.print("Press B to end.");
@@ -87,8 +99,13 @@ void loop() {
 
     // End time input
     case 'B':
-      setting_time = false;
-      displayStatus();
+      if (setting_time) {
+        is_on = true;
+        setting_time = false;
+        start_time = millis();
+        colorRGB(255, 255, 255);
+        displayStatus();
+      }
       break;
 
     // Unused keys
@@ -106,10 +123,10 @@ void loop() {
     }
   }
 
-  if (mode == TIME) {
-    // TODO
+  if (is_on && (mode == TIME)) {
+    is_on = doTime();
   } else if (mode == LIGHT) {
-    // TODO
+    doLight();
   }
 }
 
@@ -117,19 +134,42 @@ void loop() {
 void displayStatus() {
   char str[LCD_WIDTH + 1];
 
-  if (mode == TIME) {
-    sprintf(str, "Mode: Time %dm", timeout);
-  } else if (mode == LIGHT) {
-    sprintf(str, "Mode: Light");
-  }
   lcd.clear();
-  lcd.print(str);
+  if (mode == TIME) {
+    lcd.print("Mode: Time");
+  } else if (mode == LIGHT) {
+    lcd.print("Mode: Light");
+  }
+  lcd.setCursor(0, 1);
+  lcd.print("Press A to chmod");
 }
 
-void doTime() {
-  // TODO
+bool doTime() {
+  if (millis() - start_time > timeout * 60000UL) {
+    Serial.println("timeout in doTime ...");
+    colorRGB(0, 0, 0);
+    return false;
+  }
+  return true;
 }
 
 void doLight() {
-  // TODO
+  static unsigned long last;
+  unsigned long current;
+  int val;
+
+  if ((current = millis()) - last > 1000) {
+    Serial.println("doLight ...");
+    last = current;
+    val = analogRead(PHOTOR);
+    Serial.println(val);
+    int brightness = map(val, VMIN, VMAX, 0, 255);
+    colorRGB(brightness, brightness, brightness);
+  }
+}
+
+void colorRGB(int red, int green, int blue) {
+  analogWrite(RED, red);
+  analogWrite(GREEN, green);
+  analogWrite(BLUE, blue);
 }
